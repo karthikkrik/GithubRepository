5315fdc31186d41fb7b96ee8299b2adb75540b21
karthikkrik <karthik.gandi@thirdware.com>
2018-10-12 14:59:23 +0530

    RemovePalindrome
    Written a method to reverse a string.
    passing original array string to strRev method and comparing with original array string.

---
+++ JavaJ2eePattern/src/com/task/PalinRemov.java
@@ -0,0 +1,33 @@
+package com.task;
+
+import java.util.Scanner;
+
+public class PalinRemov {
+	
+	public static String strRev(String str){
+		char[] ch = str.toCharArray();
+		String rev = "";
+		for (int i=ch.length-1;i>=0;i--){
+			rev += Character.toString(ch[i]);
+		}
+		return rev;
+	}
+
+	public static void main(String[] args) {
+		
+		Scanner sc  = new Scanner(System.in);
+		System.out.print("Enter the input string: ");
+		String str  = sc.nextLine();
+		String strresult = "";
+		sc.close();
+		String [] strArr  = (str.split(" "));
+	
+	for(int i=0;i<=strArr.length-1;i++){
+		if(!(strArr[i].equalsIgnoreCase(strRev(strArr[i])))){//passing original array string to strRev method
+				strresult = strresult +strArr[i]+" ";// and comparing with original array string.
+			}
+		}
+		System.out.println(strresult);
+	}
+
+}
